c4df9b7ec5829eef43c4fd7d12a7071d5a70957d
Vainioleo <leo.vainio@solita.fi>
2025-01-16 15:21:08 +0200

    muuta että ottaa myös muita kuin divejä talteen

--- content.js
+++ content.js
@@ -10,15 +10,15 @@ chrome.runtime.onMessage.addListener((message) => {
 });
 
 document.addEventListener('mouseover', function(event) {
-    if (selectionEnabled && event.target.tagName.toLowerCase() === 'div') {
-        console.log('Mouseover div:', event.target);
-        event.target.style.border = '1px solid red'; // Made border red for visibility
+    if (selectionEnabled && event.target.tagName) {
+        console.log('Mouseover element:', event.target);
+        event.target.style.border = '1px solid red';
     }
 });
 
 document.addEventListener('mouseout', function(event) {
-    if (selectionEnabled && event.target.tagName.toLowerCase() === 'div') {
-        console.log('Mouseout div:', event.target);
+    if (selectionEnabled && event.target.tagName) {
+        console.log('Mouseout element:', event.target);
         event.target.style.border = '';
     }
 });
